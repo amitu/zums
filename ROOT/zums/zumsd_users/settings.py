e8fffab0abb7a66162f1d872d64642158d9c02ca
Amit Upadhyay <comments@amitu.com>
2011-03-27 10:18:49 -0400

    removing registration app, zums_users will be used instead

---
+++ ROOT/zums/zumsd_users/settings.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = '/tmp/zums_jd_test_project.db'
+INSTALLED_APPS = ['zums_jd_test_project']
+ROOT_URLCONF = 'zums_jd_test_project.urls'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+)
+SESSION_ENGINE = "zums.django.session_engine"
+SECRET_KEY = 'gw2)POSOBX1Z1N4NQ-oh3kv%atu+awg@=@r!jklo@i_v&#o'
+
