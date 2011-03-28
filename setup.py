5978538724ba1f3ab0e4b95e686c5bf7edb70c44
Amit Upadhyay <comments@amitu.com>
2011-03-28 09:20:47 -0400

    starting docs.. ./bin/docs now builds docs

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zums",
-    version = "0.1",
+    version = "0.1.2",
     url = 'http://github.com/amitu/zums',
     license = 'BSD',
-    description = file("README.rst").read(),
+    description = "ZeroMQ based User and Session management for webapps in different languages/frameworks",
+    long_description = file("README.rst").read(),
     author = 'Amit Upadhyay',
+    author_email = "upadhyay@gmail.com",
     packages = find_packages('ROOT'),
     package_dir = {'': 'ROOT'},
     install_requires = ['setuptools'],
