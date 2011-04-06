d7ba98c1bdf01d4c78094a1d32fa126a41787de7
Amit Upadhyay <comments@amitu.com>
2011-04-06 14:52:06 -0400

    buildout and fabfile improvements

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,6 @@ from fabric.api import local
 def docs():
     local("./bin/docs")
     local("./bin/python setup.py upload_sphinx --upload-dir=docs/html")
+
+def release():
+    local("./bin/python setup.py sdist --formats=gztar,zip upload")
